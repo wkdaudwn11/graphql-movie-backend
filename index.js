89fabd75283086446b67b9c42d975b8662e74708
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-13 17:04:58 +0900

    schema, resolver 작성 및 서버 실행

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
-
+import resolvers from "./graphql/resolvers";
 /**
  * Schema란?
  * 사용자에게 보내거나, 사용자로부터 받을 data에 대한 설명
  */
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+});
 
 server.start(() => console.log("Graphql Server Running.."));
