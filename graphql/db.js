17cf7ee376ca0d0c7d5b079b749678e496c6ea25
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-14 15:22:41 +0900

    https://yts.mx 의 API를 이용하여 영화 목록 불러오는 API 작성 (node-fetch 사용)

--- graphql/db.js
+++ graphql/db.js
@@ -1,75 +1,13 @@
-// 영화 목록
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1,
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "The Godfather 1",
-    score: 99,
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2,
-  },
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
 /**
- * @desc  영화 목록을 리턴해주는 함수
+ * @desc  영화 목록
+ * @param {Number} limit
+ * @param {Number} rating
  */
-export const getMovies = () => movies;
-
-/**
- * @desc  영화 검색
- * @param {Number} id
- */
-export const searchMovieGetById = (id) => {
-  const filteredMovies = movies.filter((movie) => id === movie.id);
-  return filteredMovies[0];
-};
-
-/**
- * @desc  영화 삭제
- * @param {Number} id
- */
-export const deleteMovie = (id) => {
-  const cleanMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanMovies.length) {
-    movies = cleanMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-/**
- * @desc  movies 배열의 마지막 요소를 리턴해주는 함수
- */
-const getLastMovie = () => {
-  return movies[movies.length - 1];
-};
-
-/**
- * @desc  영화 추가
- * @param {String}  name
- * @param {Number}  score
- */
-export const addMovie = (name, score) => {
-  if (!name || !score) return;
-  const lastMovie = getLastMovie();
-  const newMovie = {
-    id: lastMovie.id + 1,
-    name,
-    score,
-  };
-  movies = movies.concat(newMovie);
-  return newMovie;
+export const getMovies = (limit, rating) => {
+  return fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMovies, searchMovieGetById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
-    movie: (_, { id }) => searchMovieGetById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
